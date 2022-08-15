ecb74a33c478dba77795953b22d08f9860ea69d1
Michael Nunez <michael.nunez@uepa.com>
2022-08-15 14:50:48 -0400

    Commit Inicial

---
+++ BlazorServerIdiomas/Controllers/CulturaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerIdiomas.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CulturaController : Controller
+    {
+        public IActionResult SetearCultura(string cultura, string redireccionURL)
+        {
+            if (cultura != null)
+            {
+                HttpContext.Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(
+                        new RequestCulture(cultura)
+                        )
+                    );
+            }
+
+            return LocalRedirect(redireccionURL);
+        }
+    }
+}
---
+++ BlazorServerIdiomas/Helpers/Constantes.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace BlazorServerIdiomas.Helpers
+{
+    public static class Constantes
+    {
+        public static CultureInfo[] CulturasSoportadas = new[]
+             {
+                new CultureInfo("en-US"),
+                new CultureInfo("es-US"),
+                new CultureInfo("es-DO"),
+                new CultureInfo("es"),
+                new CultureInfo("en")
+            };
+    }
+}
